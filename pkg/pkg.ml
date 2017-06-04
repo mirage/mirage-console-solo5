cdb45eab55a12a99a2a0e3db4d098d404340e3fd
Hannes Mehnert <hannes@mehnert.org>
2017-06-04 16:48:20 +0200

    write_one: block until all the bytes are written

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,9 +4,7 @@
 open Topkg
 
 let () =
-  let lint_deps_excluding =
-    Some ["mirage-solo5" ; "mirage-types-lwt" ; "mirage-types" ]
-  in
+  let lint_deps_excluding = Some ["mirage-solo5" ] in
   let opams = [ Pkg.opam_file "opam" ~lint_deps_excluding ] in
   Pkg.describe ~opams "mirage-console-solo5" @@ fun c ->
   Ok [ Pkg.mllib "src/mirage-console-solo5.mllib"; ]
